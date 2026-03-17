8c25548aac2994436da28c6e897e23b93ab61111
estiven-4429 <mahechahe@gmail.com>
2026-03-17 14:44:24 -0500

    fix: update revision number for precached index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.1mkj3j1a7g"
+    "revision": "0.86o1r03a1p"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
